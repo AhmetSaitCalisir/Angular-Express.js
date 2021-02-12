8c7e6fc2de00f01973cf4e515c8446651b4991de
AhmetSaitCalisir <56252214+AhmetSaitCalisir@users.noreply.github.com>
2021-02-12 14:52:34 +0300

    v0.1.5
    -CLIENT-
    
    Navbar component'i eklendi

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductItemsComponent } from './components/product-items/product-items.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductItemsComponent, ProductItemComponent],
+  declarations: [AppComponent, ProductItemsComponent, ProductItemComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
